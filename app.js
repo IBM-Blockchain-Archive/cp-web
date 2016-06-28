db5cfc89d26009605124ec4bf3a52c4e563ca90b
shparikh <shparikh@us.ibm.com>
2016-06-28 15:51:03 -0400

    initial changes - not tested

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use('/cc/summary', serve_static(path.join(__dirname, 'cc_summaries')));												//for chaincode investigator
-app.use(serve_static(path.join(__dirname, 'public'), {maxAge: '1d', setHeaders: setCustomCC}));							//1 day cache
+app.use(serve_static(path.join(__dirname, 'public'), { maxAge: '1d', setHeaders: setCustomCC }));							//1 day cache
 //app.use( serve_static(path.join(__dirname, 'public')) );
-app.use(session({secret: 'Somethignsomething1234!test', resave: true, saveUninitialized: true}));
+app.use(session({ secret: 'Somethignsomething1234!test', resave: true, saveUninitialized: true }));
 function setCustomCC(res, path) {
-    if (serve_static.mime.lookup(path) === 'image/jpeg')  res.setHeader('Cache-Control', 'public, max-age=2592000');		//30 days cache
+    if (serve_static.mime.lookup(path) === 'image/jpeg') res.setHeader('Cache-Control', 'public, max-age=2592000');		//30 days cache
     else if (serve_static.mime.lookup(path) === 'image/png') res.setHeader('Cache-Control', 'public, max-age=2592000');
     else if (serve_static.mime.lookup(path) === 'image/x-icon') res.setHeader('Cache-Control', 'public, max-age=2592000');
 }
@@ -71,9 +71,9 @@ app.use(function (req, res, next) {
     var url_parts = url.parse(req.url, true);
     req.parameters = url_parts.query;
     keys = Object.keys(req.parameters);
-    if (req.parameters && keys.length > 0) console.log({parameters: req.parameters});		//print request parameters
+    if (req.parameters && keys.length > 0) console.log({ parameters: req.parameters });		//print request parameters
     keys = Object.keys(req.body);
-    if (req.body && keys.length > 0) console.log({body: req.body});						//print request body
+    if (req.body && keys.length > 0) console.log({ body: req.body });						//print request body
     next();
 });
 
@@ -93,9 +93,9 @@ app.use(function (err, req, res, next) {		// = development error handler, print 
     console.log("Error Handeler -", req.url);
     var errorCode = err.status || 500;
     res.status(errorCode);
-    req.bag.error = {msg: err.stack, status: errorCode};
+    req.bag.error = { msg: err.stack, status: errorCode };
     if (req.bag.error.status == 404) req.bag.error.msg = "Sorry, I cannot locate that file";
-    res.render('template/error', {bag: req.bag});
+    res.render('template/error', { bag: req.bag });
 });
 
 // Track the application deployments
@@ -206,6 +206,94 @@ configure_network();
 // ==================================
 // configure ibm-blockchain-js sdk
 // ==================================
+var hlc = require('hlc');
+var chain = hlc.newChain("cp-web");
+
+
+function configure_network() {
+    var registrar = {
+        name: 'WebAppAdmin',
+        secret: 'DJY27pEnl16d'
+    };
+    chain.setKeyValStore(hlc.newFileKeyValStore('/tmp/keyValStore'));
+    if (fs.existsSync("tlsca.cert")) {
+        chain.setMemberServicesUrl("grpcs://test-ca.rtp.raleigh.ibm.com:50051", fs.readFileSync('tlsca.cert'));
+    } else {
+        chain.setMemberServicesUrl("grpc://test-ca.rtp.raleigh.ibm.com:50051");
+    }
+    chain.addPeer("grpc://test-peer1.rtp.raleigh.ibm.com:30303");
+    chain.addPeer("grpc://test-peer2.rtp.raleigh.ibm.com:30303");
+    chain.addPeer("grpc://test-peer3.rtp.raleigh.ibm.com:30303");
+    chain.setDevMode(true);
+    var testChaincodePath = "github.com/cp-chaincode-v2/";
+    var testChaincodeID = "cp-chaincode";
+
+    chain.getMember("WebAppAdmin", function (err, WebAppAdmin) {
+        if (err) {
+            console.log("Failed to get WebAppAdmin member " + " ---> " + err);
+            t.end(err);
+        } else {
+            console.log("Successfully got WebAppAdmin member" + " ---> " /*+ JSON.stringify(crypto)*/);
+
+            // Enroll the WebAppAdmin member with the certificate authority using
+            // the one time password hard coded inside the membersrvc.yaml.
+            pw = "DJY27pEnl16d";
+            WebAppAdmin.enroll(pw, function (err, crypto) {
+                if (err) {
+                    console.log("Failed to enroll WebAppAdmin member " + " ---> " + err);
+                    t.end(err);
+                } else {
+                    console.log("Successfully enrolled WebAppAdmin member" + " ---> " /*+ JSON.stringify(crypto)*/);
+
+                    // Confirm that the WebAppAdmin token has been created in the key value store
+                    path = chain.getKeyValStore().dir + "/member." + WebAppAdmin.getName();
+
+                    fs.exists(path, function (exists) {
+                        if (exists) {
+                            console.log("Successfully stored client token for" + " ---> " + WebAppAdmin.getName());
+                        } else {
+                            console.log("Failed to store client token for " + WebAppAdmin.getName() + " ---> " + err);
+                        }
+                    });
+                }
+                chain.setRegistrar(WebAppAdmin);
+                deploy(WebAppAdmin);
+            });
+        }
+    });
+}
+
+function deploy(WebAppAdmin) {
+    var deployRequest = {
+        // Name (hash) required for invoke
+        chaincodeID: testChaincodeID,
+        // Function to trigger
+        fcn: "init",
+        // Parameters for the invoke function
+        args: []
+    };
+
+    // Trigger the invoke transaction
+    var deployTx = WebAppAdmin.deploy(deployRequest);
+
+    // Print the invoke results
+    deployTx.on('submitted', function (results) {
+        // Invoke transaction submitted successfully
+        console.log("Successfully submitted chaincode deploy transaction" + " ---> " + "function: " + deployRequest.function + ", args: " + deployRequest.arguments + " : " + results);
+    });
+
+    // Listen for the completed event
+    deployTx.on('complete', function (results) {
+        // Invoke transaction submitted successfully
+        console.log("Successfully completed chaincode deploy transaction" + " ---> " + "function: " + deployRequest.function + ", args: " + deployRequest.arguments + " : " + results);
+    });
+
+    deployTx.on('error', function (err) {
+        // Invoke transaction submission failed
+        console.log("Failed to submit chaincode invoke transaction" + " ---> " + "function: " + deployRequest.function + ", args: " + deployRequest.arguments + " : " + err);
+    });
+}
+/*
 function configure_network() {
 
     options = {
@@ -247,12 +335,13 @@ function cb_ready(err, cc) {//response has chaincode functions
         }
     }
 }
-
+*/
 /**
  * Configures other parts of the app that depend on the blockchain network being configured and running in
  * order to function.
  * @param err Will capture any errors from deploying the chaincode.
  */
+/*
 function finalSetup(err, data) {
     if (err != null) {
         //look at tutorial_part1.md in the trouble shooting section for help
@@ -263,7 +352,7 @@ function finalSetup(err, data) {
         user_manager.setup(ibc, chaincode, ca, cb_deployed);
     }
 }
-
+*/
 // ============================================================================================================================
 // 												WebSocket Communication Madness
 // ============================================================================================================================
@@ -271,12 +360,12 @@ function cb_deployed(e, d) {
     if (e != null) {
         //look at tutorial_part1.md in the trouble shooting section for help
         console.log('! looks like the final configuration failed, holding off on the starting the socket\n', e);
-        if (!process.error) process.error = {type: 'deploy', msg: e.details};
+        if (!process.error) process.error = { type: 'deploy', msg: e.details };
     }
     else {
         console.log('------------------------------------------ Websocket Up ------------------------------------------');
         ibc.save('./cc_summaries');															//save it here for chaincode investigator
-        wss = new ws.Server({server: server});												//start the websocket now
+        wss = new ws.Server({ server: server });												//start the websocket now
         wss.on('connection', function connection(ws) {
             ws.on('message', function incoming(message) {
                 console.log('received ws msg:', message);
@@ -307,14 +396,14 @@ function cb_deployed(e, d) {
             if (chain_stats && chain_stats.height) {
                 console.log('hey new block, lets refresh and broadcast to all');
                 ibc.block_stats(chain_stats.height - 1, cb_blockstats);
-                wss.broadcast({msg: 'reset'});
+                wss.broadcast({ msg: 'reset' });
                 chaincode.query.query(['GetAllCPs'], cb_got_papers);
             }
 
             //got the block's stats, lets send the statistics
             function cb_blockstats(err, stats) {
                 if (chain_stats.height) stats.height = chain_stats.height - 1;
-                wss.broadcast({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
+                wss.broadcast({ msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats });
             }
 
             function cb_got_papers(e, papers) {
@@ -323,7 +412,7 @@ function cb_deployed(e, d) {
                 }
                 else {
                     //console.log('papers', papers);
-                    wss.broadcast({msg: 'papers', papers: papers});
+                    wss.broadcast({ msg: 'papers', papers: papers });
                 }
             }
 
@@ -332,7 +421,7 @@ function cb_deployed(e, d) {
                 if (e != null) console.log('error:', e);
                 else {
                     if (trades && trades.open_trades) {
-                        wss.broadcast({msg: 'open_trades', open_trades: trades.open_trades});
+                        wss.broadcast({ msg: 'open_trades', open_trades: trades.open_trades });
                     }
                 }
             }
