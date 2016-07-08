191aa36895425896c6ad4042904a18249107ab5e
shparikh <shparikh@us.ibm.com>
2016-07-08 13:42:56 -0400

    random

--- utils/ws_part2.js
+++ utils/ws_part2.js
@@ -67,7 +67,7 @@ module.exports.process_msg = function (ws, data) {
                 Request.fcn = 'query';
                 Request.args = ['GetAllCPs', data.user];
 
-                data.user.setTCertBatchSize(1);
+                usr.setTCertBatchSize(1);
                 var queryTx = usr.query(Request);
 
                 // Print the query results
@@ -161,7 +161,7 @@ module.exports.process_msg = function (ws, data) {
                 Request.fcn = 'query';
                 Request.args = ['GetCompany', data.company];
 
-                data.user.setTCertBatchSize(1);
+                usr.setTCertBatchSize(1);
                 var queryTx = usr.query(Request);
 
                 // Print the query results
