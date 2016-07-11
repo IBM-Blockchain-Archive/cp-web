19773f0abe5852eea74fdb83f691e7a7f00b576b
shparikh <shparikh@us.ibm.com>
2016-07-11 11:36:57 -0400

    random

--- utils/users.js
+++ utils/users.js
@@ -180,30 +180,35 @@ function login2(id, secret, cb) {
 */
 function registerUser(username, role, cb) {
     chain.getMember(username, function (err, usr) {
-        console.log("registering user..........");
-        var registrationRequest = {
-            enrollmentID: username,
-            account: "bank_a",
-            affiliation: "00001"
-        };
-        usr.register(registrationRequest, function (err, enrollsecret) {
-            if (err) {
-                cb(err, null);
-            } else {
-                var cred = {
-                    id: username,
-                    secret: enrollsecret
-                }
-                login(cred.id, cred.secret, function (err) {
-                    if (err != null) {
-                        cb(err, null);
-                    } else {
-                        cb(null, cred);
+        if (err || err == null) {
+            console.log("registering user..........");
+            var registrationRequest = {
+                enrollmentID: username,
+                account: "bank_a",
+                affiliation: "00001"
+            };
+            usr.register(registrationRequest, function (err, enrollsecret) {
+                if (err) {
+                    cb(err, null);
+                } else {
+                    var cred = {
+                        id: username,
+                        secret: enrollsecret
                     }
-                });
+                    login(cred.id, cred.secret, function (err){
+                        if(err != null) {
+                            cb(err, null);
+                        } else {
+                            cb(null, cred);
+                        }
+                    });     
 
-            }
-        });
+                }
+            });
+        } else {
+            console.log("User alreay exists. Please login.");
+            //cb("User already exist", null);
+        }
     });
 }
 /*
