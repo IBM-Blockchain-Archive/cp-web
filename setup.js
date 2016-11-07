bfc3f73742a8a7719805bbd935b79ea58a58b777
Dale Avery <davery@us.ibm.com>
2016-11-07 15:59:33 -0500

    Consistent formatting

--- setup.js
+++ setup.js
@@ -2,60 +2,65 @@
 /*******************************************************************************
  * Copyright (c) 2015 IBM Corp.
  *
- * All rights reserved. 
+ * All rights reserved.
+ *
+ * This app will run in one of 3 environments:
+ * 1. Bluemix Production
+ * 2. Bluemix Development
+ * 3. Localhost Development
+ *
+ * This file will export objects including the port that the application should
+ * listen on.  If the application is running on the localhost, port 3000 will be
+ * used.
  *
  * Contributors:
  *   David Huffman - Initial implementation
  *******************************************************************************/
-//Environments are either:
-// 	1 - Bluemix Production
-// 	2 - Bluemix Development
-// 	3 - Localhost Development
 
-var vcap_app = {application_uris: ['']};						//default blank
+var vcap_app = {application_uris: ['']};
 var ext_uri = '';
-if(process.env.VCAP_APPLICATION){
-	vcap_app = JSON.parse(process.env.VCAP_APPLICATION);
-	for(var i in vcap_app.application_uris){
-		if(vcap_app.application_uris[i].indexOf(vcap_app.name) >= 0){
-			ext_uri = vcap_app.application_uris[i];
-		}
-	}
+if (process.env.VCAP_APPLICATION) {
+    vcap_app = JSON.parse(process.env.VCAP_APPLICATION);
+    for (var i in vcap_app.application_uris) {
+        if (vcap_app.application_uris[i].indexOf(vcap_app.name) >= 0) {
+            ext_uri = vcap_app.application_uris[i];
+        }
+    }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////    1. Bluemix Production    ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-if(process.env.VCAP_APP_HOST && process.env.PRODUCTION){
-	exports.SERVER = 	{	
-							HOST: process.env.VCAP_APP_HOST,
-							PORT: process.env.VCAP_APP_PORT,
-							DESCRIPTION: 'Bluemix - Production',
-							EXTURI: ext_uri,
-						};
+if (process.env.VCAP_APP_HOST && process.env.PRODUCTION) {
+    exports.SERVER = {
+        HOST: process.env.VCAP_APP_HOST,
+        PORT: process.env.VCAP_APP_PORT,
+        DESCRIPTION: 'Bluemix - Production',
+        EXTURI: ext_uri
+    };
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////    2. Bluemix Development    ///////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-else if(process.env.VCAP_APP_HOST){
-		exports.SERVER = 	{	
-								HOST: process.env.VCAP_APP_HOST,
-								PORT: process.env.VCAP_APP_PORT,
-								DESCRIPTION: 'Bluemix - Development',
-								EXTURI: ext_uri,
-							 };
+else if (process.env.VCAP_APP_HOST) {
+    exports.SERVER = {
+        HOST: process.env.VCAP_APP_HOST,
+        PORT: process.env.VCAP_APP_PORT,
+        DESCRIPTION: 'Bluemix - Development',
+        EXTURI: ext_uri
+    };
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////     3. Localhost - Development    ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-else{
-	exports.SERVER = 	{
-							HOST:'localhost',
-							PORT: 3000,
-							DESCRIPTION: 'Localhost',
-							EXTURI: process.env.EXTURI || 'localhost:3000',
-						 };
+else {
+    exports.SERVER = {
+        HOST: 'localhost',
+        PORT: 3000,
+        DESCRIPTION: 'Localhost',
+        EXTURI: process.env.EXTURI || 'localhost:3000'
+    };
 }
 
 exports.SERVER.vcap_app = vcap_app;
@@ -64,5 +69,4 @@ exports.SERVER.vcap_app = vcap_app;
 ////////////////////////////////////////     Common     ////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
 exports.DEBUG = vcap_app;
-exports.USER1 = 'bob';									//left username
-exports.USER2 = 'leroy';								//right username
+
