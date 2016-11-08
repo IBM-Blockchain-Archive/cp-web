860df6267d64a3a1d40b497406365b6e4c6b0156
Dale Avery <davery@us.ibm.com>
2016-11-08 18:40:54 -0500

    User manager shouldn't be responsible for chaincode operations

--- utils/users.js
+++ utils/users.js
@@ -7,6 +7,8 @@
  * This module assists with the user management for the blockchain network. It has
  * code for registering a new user on the network and logging in existing users.
  *
+ * TODO refactor this into an object.
+ *
  * Contributors:
  *   David Huffman - Initial implementation
  *   Dale Avery
@@ -14,16 +16,9 @@
  * Created by davery on 3/16/2016.
  *******************************************************************************/
 var chain;
-var chaincodeID;
-var util = require('util');
-var fs = require('fs');
 
 // Use a tag to make logs easier to find
 var TAG = 'user_manager:';
-
-function isConfigured() {
-    return chaincodeID && chain;
-}
 
 /**
  * Whoever configures the hfc chain object needs to send it here in order for this user manager to function.
@@ -34,13 +29,11 @@ function isConfigured() {
 module.exports.setup = function (ccID, ch, cb) {
     if (ch && ccID) {
         console.log(TAG, 'user manager properly configured');
-        chaincodeID = ccID;
         chain = ch;
         cb(null);
     } else {
         console.error(TAG, 'user manager was not given proper configuration');
-        var err = new Error('User manager requires both a chain object and ' +
-            'a chaincode object before it can function.');
+        var err = new Error('User manager requires a chain object.');
         cb(err);
     }
 };
@@ -49,60 +42,32 @@ module.exports.setup = function (ccID, ch, cb) {
  * Mimics a login process by attempting to register a given id and secret against
  * the first peer in the network. 'Successfully registered' and 'already logged in'
  * are considered successes.  Everything else is a failure.
- * @param id The user to log in.
- * @param secret The secret that was given to this user when registered against the CA.
+ * @param enrollID The user to log in.
+ * @param enrollSecret The secret that was given to this user when registered against the CA.
  * @param cb A callback of the form: function(err)
  */
-module.exports.login = function (id, secret, cb) {
-    console.log(TAG, 'login called');
+module.exports.login = function (enrollID, enrollSecret, cb) {
+    console.log(TAG, 'login() called');
 
-    if (!isConfigured()) {
+    if (!chain) {
         cb(new Error('Cannot enroll a user before setup() is called.'));
         return;
     }
 
-    chain.getMember(id, function (err, usr) {
-        if (err) {
-            console.log(TAG, 'Failed to get' + id + 'member ' + ' ---> ' + err);
-            if (cb) cb(err);
+    chain.getMember(enrollID, function (getError, usr) {
+        if (getError) {
+            console.log(TAG, 'getMember() failed for \"' + enrollID + '\":', getError.message);
+            if (cb) cb(getError);
         } else {
-            console.log(TAG, 'Successfully got ' + id + ' member');
+            console.log(TAG, 'Successfully got member:', + enrollID);
 
-            // Enroll the user member with the certificate authority using
-            // the one time password hard coded inside the membersrvc.yaml.
-            usr.enroll(secret, function (err, crypto) {
-                if (err) {
-                    console.log('Failed to enroll' + id + 'member ' + ' ---> ' + err);
-                    if (cb) cb(err);
+            usr.enroll(enrollSecret, function (enrollError, crypto) {
+                if (enrollError) {
+                    console.log('enroll() failed for \"' + enrollID + '\":', enrollError.message);
+                    if (cb) cb(enrollError);
                 } else {
-                    console.log('Successfully enrolled' + id + 'member' /*+ ' ---> ' + JSON.stringify(crypto)*/);
-
-                    // Confirm that the user token has been created in the key value store
-                    var path = chain.getKeyValStore().dir + '/member.' + usr.getName();
-
-                    fs.exists(path, function (exists) {
-                        if (exists) {
-                            console.log('Successfully stored client token' /*+ ' ---> ' + user.getName()*/);
-                        } else {
-                            console.log('Failed to store client token for ' + usr.getName() + ' ---> ' + err);
-                        }
-                    });
-                    var Request = {
-                        chaincodeID: chaincodeID,
-                        fcn: 'createAccount',
-                        args: [id]
-                    };
-                    var invokeTx = usr.invoke(Request);
-                    invokeTx.on('submitted', function (results) {
-                        // Invoke transaction submitted successfully
-                        console.log(util.format('Successfully submitted chaincode invoke transaction: request=%j, response=%j', Request, results));
-                        if (cb) cb(null);
-                    });
-                    invokeTx.on('error', function (err) {
-                        // Invoke transaction submission failed
-                        console.log(util.format('Failed to submit chaincode invoke transaction: request=%j, error=%j', Request, err));
-                        if (cb) cb(err);
-                    });
+                    console.log('Successfully enrolled \"' + enrollID + '\"');
+                    if(cb) cb(null);
                 }
             });
         }
@@ -117,7 +82,7 @@ module.exports.login = function (id, secret, cb) {
 module.exports.registerUser = function (enrollID, cb) {
     console.log(TAG, 'registerUser() called');
 
-    if (!isConfigured()) {
+    if (!chain) {
         cb(new Error('Cannot register a user before setup() is called.'));
         return;
     }
